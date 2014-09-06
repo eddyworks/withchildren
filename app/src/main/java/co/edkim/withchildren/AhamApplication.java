4bb74704901e68be2ac77c02075c1bd774b94c99
Jobs Kim <ed.kim@edkim.co>
2014-09-06 12:17:49 +0900

    Google 분석 추가.

---
+++ app/src/main/java/co/edkim/withchildren/AhamApplication.java
@@ -0,0 +1,38 @@
+package co.edkim.withchildren;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
+
+/**
+ * Created by jiminkim on 2014. 9. 6..
+ */
+public class AhamApplication extends Application {
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a company.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker("UA-44484721-5")
+                    : analytics.newTracker(R.xml.global_tracker);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+}
